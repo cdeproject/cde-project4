d123b41bab0794322677650ce4e86daf0941eabb
CDE Test <cde_test@example.com>
2022-11-24 21:11:47 +0700

    Init

---
+++ run_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag my-python-app .
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app
+docker run -p 8000:80 --name my-running-app my-python-app
